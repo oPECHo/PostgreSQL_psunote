cbb44d1bfa2e92d6617874588076cfa3a50dfbf9
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-10-03 11:34:40 +0700

    Create route function Update and Delete

--- psunote/noteapp.py
+++ psunote/noteapp.py
@@ -3,6 +3,7 @@ import flask
 import models
 import forms
 
+from sqlalchemy.sql import func
 
 app = flask.Flask(__name__)
 app.config["SECRET_KEY"] = "This is secret key"
@@ -75,6 +76,50 @@ def tags_view(tag_name):
         tag_name=tag_name,
         notes=notes,
     )
+
+@app.route("/notes/<int:note_id>/", methods=["GET", "POST"])
+def notes_update(note_id):
+    form = forms.NoteForm()
+    db = models.db
+    note = db.session.query(models.Note).get(note_id)
+    if note:
+        if form.validate_on_submit():
+            note.title = form.title.data
+            note.description = form.description.data
+            note.updated_date = func.now()
+            note.tags.clear()
+            for tag_name in form.tags.data:
+                tag = (
+                    db.session.execute(db.select(models.Tag).where(models.Tag.name == tag_name))
+                    .scalars()
+                    .first()
+                )
+                if not tag:
+                    tag = models.Tag(name=tag_name)
+                    db.session.add(tag)
+                note.tags.append(tag)
+            db.session.commit()
+            return flask.redirect(flask.url_for("index"))
+        
+        form.title.data = note.title
+        form.description.data = note.description
+        form.tags.data = [tag.name for tag in note.tags]
+
+    return flask.render_template(
+        "notes-edit.html",
+        note=note,
+        form=form,
+    )
+
+@app.route("/notes/<int:note_id>/delete", methods=["GET", "POST"])
+def notes_delete(note_id):
+    db = models.db
+    note = db.session.query(models.Note).get(note_id)
+    db.session.delete(note)
+    db.session.commit()
+
+    return flask.redirect(flask.url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
